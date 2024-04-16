97b537a8ec96f18b2eba8cd17d4893eb48cb78c4
vatsalrami <vatsalrami@gmail.com>
2024-04-16 13:51:05 -0400

    firebase auth in env

--- src/backend/database/firebase.js
+++ src/backend/database/firebase.js
@@ -1,7 +1,8 @@
 const admin = require("firebase-admin");
+require("dotenv").config();
 
 // Path to your JSON file
-const serviceAccount = require("../../../firebase-credentials.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
